ee17fc0d3ffe7b61e70e43a463afe625700ce355
alexsc90 <a.s.cebreros@gmail.com>
2021-07-03 00:34:56 -0500

    servidor web estatico creado con express

---
+++ platziverse-web/server.js
@@ -0,0 +1,42 @@
+'use strict'
+
+const debug = require('debug')('platziverse:web')
+const http = require('http')
+const path = require('path')
+const express = require('express')
+const socketio = require('socket.io')
+const chalk = require('chalk')
+const dotenv = require('dotenv') 
+
+const port = process.env.PORT || 5000
+const app = express()
+const server = http.createServer(app)
+const io = socketio(server)
+
+app.use(express.static(path.join(__dirname, 'public')))
+
+// Socket.io / WebSockets
+io.on('connect', socket => {
+  debug(`Connected ${socket.id}`)
+
+  socket.on('agent/message', payload => {
+    console.log(payload)
+  })
+
+  setInterval(() => {
+    socket.emit('agent/message', { agent: 'xxx-yyy' })
+  }, 2000)
+})
+
+function handleFatalError (err) {
+  console.error(`${chalk.red('[fatal error]')} ${err.message}`)
+  console.error(err.stack)
+  process.exit(1)
+}
+
+process.on('uncaughtException', handleFatalError)
+process.on('unhandledRejection', handleFatalError)
+
+server.listen(port, () => {
+  console.log(`${chalk.green('[platziverse-web]')} server listening on port ${port}`)
+})
